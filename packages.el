1940da4c910250d143f731032f60f3cdc53856f8
firaaz <36291085+firaaz@users.noreply.github.com>
2019-10-30 20:30:41 +0530

    Update packages.el

--- packages.el
+++ packages.el
@@ -1,9 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-
-(package! olivetti)
+(package! writeroom-mode)
+(package! helm-spotify-plus)
+(package! evil-matchit)
