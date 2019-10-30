4626f2f5339591e532737268a23ac8f364ccc46e
firaaz <36291085+firaaz@users.noreply.github.com>
2019-10-30 20:34:05 +0530

    Update config.el

--- config.el
+++ config.el
@@ -1,43 +1,34 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; .doom.d/config.el -*- lexic
+
 ;; Place your private configuration here
 
-;; user defined functions
-(defun newline-below ()
-  (interactive)
-  (let ((oldpos (point)))
-    (end-of-line)
-    (newline)))
+;; setup
+(setq whitespace-line-column 80)
 
-(defun newline-above ()
-  (interactive)
-  (let ((oldpos (point)))
-    (beginning-of-line)
-    (newline)))
-
-(setq which-key-idle-delay 0.5)
+(require 'company)
 (setq company-idle-delay 0.2
       company-minimum-prefix-length 2)
 
-;; hooks
-(after! anaconda-mode
-  (set-company-backend! 'anaconda-mode '(company-anaconda company-yasnippet)))
+(require 'evil-matchit)
+(global-evil-matchit-mode 1)
 
-;; keybindings
-(map! :prefix "SPC"
-      :n "o m" #'counsel-rhythmbox
-      :n "o w" #'olivetti-mode)
+(setq which-key-idle-delay 0.5)
 
-;;; movement keybindings
-(map! :ni "C-h" #'evil-window-left
+
+;; Keybindings
+(map! :n "SPC o w" #'writeroom-mode
+      :n "SPC o m" #'counsel-rhythmbox
+      :n "SPC o s" #'helm-spotify-plus
+      :ni "C-h" #'evil-window-left
       :ni "C-j" #'evil-window-down
       :ni "C-k" #'evil-window-up
-      :ni "C-l" #'evil-window-right
-      :nv "] SPC" #'newline-below
-      :nv "[ SPC" #'newline-above)
+      :ni "C-l" #'evil-window-right)
 
-;; settings
-(setenv "PATH" (concat (getenv "PATH") (concat ":" (getenv "HOME") "/anaconda3/bin")))
-(setq exec-path (append exec-path (concat (getenv "HOME") "/anaconda3/bin")))
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 ;;; UTF-8 support for terminal sessions
 (prefer-coding-system       'utf-8)
@@ -45,7 +36,3 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-
-
-(provide 'config)
-;;; config.el ends here
